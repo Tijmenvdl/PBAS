eafe5b58ea066b70bf360d70beb832a56e3f2f2a
Tijmen van der Looij <tijmen@3-dm.nl>
2026-04-06 19:36:55 +0200

    Mega nice, basic VRP alleen klopt nog niet helemaal visueel

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PBAS
 PBAS Work Ahold Case Group 9
+
+run `pip install -r requirements.txt` in vscode terminal before usage, to make sure the proper packages are installed. Contains a lot more than required, but thats because I froze my full local package setup into the file for my own convenience. If you add another package through your code, update the requirements folder by stating `pip freeze > requirements.txt` in terminal.
--- main.py
+++ main.py
@@ -1,10 +1,50 @@
-from data_prep import load_data, create_dist_matrix
+import folium
 
-def solve():
-    trucks, dc, df_stores, df_demand, df_distances = load_data("PBAS - Data Case AH 2026.xlsx")
-    dist_matrix, time_matrix = create_dist_matrix(df_distances, df_stores)
+from data_prep import load_data
+from basic_vrp import basic_vrp_framework
 
-    return None
+def show_solution():
+    """Uses Folium to create map visual with solution"""
+    data, routing, manager, solution = basic_vrp_framework()
+
+    _, dc, df_stores, _, _ = load_data("PBAS - Data Case AH 2026.xlsx")
+
+    depot_coords = (dc["lat"], dc["long"])
+    coord_lookup = df_stores.set_index("Store nr")[["Latitude", "Longitude"]].to_dict("index")
+
+    m = folium.Map(location=depot_coords, zoom_start=10)
+    colours = ["red", "blue", "green", "purple", "orange", "darkred", "cadetblue"]
+
+    for vehicle_id in range(data["n_vehicles"]):
+        index = routing.Start(vehicle_id)
+        route_coords = [depot_coords] # start at depot
+
+        while not routing.IsEnd(index):
+            node = manager.IndexToNode(index)
+            store_nr = data["node_index_to_store"][node]
+
+            if store_nr != 9999:
+                lat = coord_lookup[store_nr]["Latitude"]
+                lon = coord_lookup[store_nr]["Longitude"]
+                route_coords.append((lat, lon))
+                folium.Marker([lat, lon],
+                              tooltip=store_nr,
+                              icon=folium.Icon(
+                                  color=colours[vehicle_id % len(colours)]
+                              )).add_to(m)
+                
+            index = solution.Value(routing.NextVar(index))
+
+        route_coords.append(depot_coords) # close route at DC
+
+        if len(route_coords) > 2: # Add only used vehicles
+            folium.PolyLine(route_coords, color=colours[vehicle_id % len(colours)],  weight=2).add_to(m)
+
+    folium.Marker(depot_coords, tooltip="DC", icon=folium.Icon(color="black", icon="home")).add_to(m)       
+
+    m.save("routes.html")
 
 if __name__ == "__main__":
-    solve() # test
+    show_solution() # test
+
+print()
